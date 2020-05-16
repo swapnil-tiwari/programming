2dcda3dfe6d792e9649a50d2f901baf904b7b954
Swapnil Tiwari <swapnil_tiwari@outlook.com>
2020-05-16 15:15:28 +0530

    new prob

---
+++ Hackerearth/communicating-virus.py
@@ -0,0 +1,27 @@
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+def main():
+    stream=input()
+    N,M=map(int,stream.split())
+    virus=[0 for _ in range(0,N)]
+    for _ in range(0,M):
+        inp=input()
+        L,K,P=map(int,inp.split())
+        for x in range(L-1,K):
+            virus[x]+=P
+        # print(virus)
+    res=0
+    for each in virus:
+        res=res^each
+    print(res)    
+
+t=int(input())
+for _ in range(0,t):
+    main()
+# Write your code here
